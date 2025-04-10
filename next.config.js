3d7a7491fb182a7cd545c78990d409e681b1aa95
Vinay Datta <vinaydattarao@gmail.com>
2025-04-11 02:07:56 +0530

    Add: GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/college-management-system',
+  assetPrefix: '/college-management-system/'
 }
 
 module.exports = nextConfig 
